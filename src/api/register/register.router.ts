01597aa15929abc26028aca20c33c86d250e0ed6
Harsh Banka <harshbanka321@gmail.com>
2023-11-14 13:26:28 +0000

    feat: add on api

--- src/api/register/register.router.ts
+++ src/api/register/register.router.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { validateRequest } from "../../shared/middlewares/valiator";
 import { registerSchema } from "./register.schema";
-import { register } from "./register.controller";
+import { onspotRegister, register } from "./register.controller";
 
 export default (): Router => {
     const app = Router();
     app.post("/", validateRequest('body', registerSchema), register);
+    app.post("/onspot", validateRequest('body', registerSchema), onspotRegister);
     return app;
 };
