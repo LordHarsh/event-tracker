e0e1d803c4b29ed4e3b94bcfafec3b4599613511
harshbanka321 <harshbanka321@gmail.com>
2023-12-13 11:18:10 +0000

    initial

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import testRouter from "./test/test.router";
 import attendanceRouter from "./attendance/attendance.router";
 import registerRouter from "./register/register.router";
 import { checkEvent } from "../shared/middlewares/checkEvent";
@@ -7,7 +6,6 @@ import refreshRouter from "./refresh/refresh.router";
 
 export default (): Router => {
     const app = Router();
-    app.use('/test', testRouter());
     app.use('/:eventName/attendance', checkEvent, attendanceRouter());
     app.use('/:eventName/register', checkEvent, registerRouter());
     app.use('/refresh', refreshRouter());
